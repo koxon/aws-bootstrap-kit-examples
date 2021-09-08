4ea6278c2e8b9075386fd46dfa943432002eaf58
Benoit de Chateauvieux <chateauv@amazon.com>
2021-09-08 18:47:59 -0400

    Add API Usage in dashboard

--- source/5-unicornPics/backend/lib/common/monitoring.ts
+++ source/5-unicornPics/backend/lib/common/monitoring.ts
@@ -11,6 +11,7 @@ export class Monitoring extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string, props: MonitoringConfigProps) {
     super(scope, id);
 
+    //UnicornPics metrics
     function createWidget(namespace:string, metricName: string, dimensions: cloudwatch.DimensionHash) {
       const metric = new cloudwatch.Metric({
         metricName,
@@ -37,11 +38,37 @@ export class Monitoring extends cdk.Construct {
       return widget;
     }
 
+    //API usage
+    const apigwSearchMetric = new cloudwatch.MathExpression({
+      expression: "SEARCH('{AWS/ApiGateway,ApiName,Method,Resource,Stage} (Method=\"GET\" OR Method=\"PUT\") AND MetricName=\"Count\"', 'Average', 300)",
+      usingMetrics: {}, //https://github.com/aws/aws-cdk/issues/7237
+    });
+    const apigwMathMetric = new cloudwatch.MathExpression({
+      expression: "FILL(count, 0)",
+      usingMetrics: { count: apigwSearchMetric },
+      label: "Count"
+    });
+    const apigwWidget = new cloudwatch.GraphWidget({
+      title: "API usage",
+      view: cloudwatch.GraphWidgetView.TIME_SERIES,
+      stacked: true,
+      liveData: true,
+      left: [apigwMathMetric],
+      leftYAxis: {
+        showUnits: false
+      }
+    });
+
     // Create business dashboard
     const businessDashboard = new cloudwatch.Dashboard(this, "businessDashboard", {
       dashboardName: "BusinessDashboard",
     });
-    businessDashboard.addWidgets(createWidget("UnicornPics", "UploadedPics", {}), createWidget("UnicornPics", "Likes", {}), createWidget("UnicornPics", "Dislikes", {}));
-    businessDashboard.addWidgets(createWidget("AWS/Cognito", "SignUpSuccesses", {UserPool: props.auth.userPool.userPoolId, UserPoolClient: props.auth.userPoolClient.userPoolClientId}));
+    businessDashboard.addWidgets(
+      createWidget("UnicornPics", "UploadedPics", {}),
+      createWidget("UnicornPics", "Likes", {}),
+      createWidget("UnicornPics", "Dislikes", {}));
+    businessDashboard.addWidgets(
+      createWidget("AWS/Cognito", "SignUpSuccesses", {UserPool: props.auth.userPool.userPoolId, UserPoolClient: props.auth.userPoolClient.userPoolClientId}),
+      apigwWidget);
   }
 }
