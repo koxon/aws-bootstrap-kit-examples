2d7b654e18af9d62f15e89238431f78eb3bb848a
Benoit de Chateauvieux <chateauv@amazon.com>
2021-09-14 09:09:12 -0400

    Posting pic in load testing script

--- source/5-unicornPics/backend/lib/loadtesting/loadtesting.triggerLoadTest.ts
+++ source/5-unicornPics/backend/lib/loadtesting/loadtesting.triggerLoadTest.ts
@@ -1,22 +1,18 @@
 import * as AWS from 'aws-sdk';
 import { Handler } from 'aws-lambda';
 import axios from 'axios';
+const s3 = new AWS.S3();
 
-const USER_POOL_ID             = process.env.USER_POOL_ID!;
-const CLIENT_ID                = process.env.CLIENT_ID!;
-const API_URL                  = process.env.API_URL!;
+const USER_POOL_ID = process.env.USER_POOL_ID!;
+const CLIENT_ID = process.env.CLIENT_ID!;
+const API_URL = process.env.API_URL!;
+const PICTURE_BUCKET = process.env.PICTURE_BUCKET!;
+const PICTURE_KEY = process.env.PICTURE_KEY!;
 const NUMBER_OF_LIKES_PER_USER = 10
 
 export const handler: Handler = async (event) => {
     const username = event['username'];
     const password = event['password'];
-
-    console.log({
-        USER_POOL_ID,
-        CLIENT_ID,
-        API_URL,
-        username,
-    });
 
     const cognitoIsp = new AWS.CognitoIdentityServiceProvider();
     const params = {
@@ -29,19 +25,54 @@ export const handler: Handler = async (event) => {
         }
     };
     const tokens = await cognitoIsp.adminInitiateAuth(params).promise();
-    console.log(tokens);
     if (!tokens['AuthenticationResult'] || !tokens['AuthenticationResult']['IdToken']) {
         throw new Error('Missing Auth Token')
     }
+    const apiHeaders = {
+        authorization: tokens['AuthenticationResult']['IdToken'],
+        accept: 'application/json',
+        aud: CLIENT_ID,
+    }
 
-    axios.defaults.baseURL = API_URL;
-    axios.defaults.headers.common['authorization'] = tokens['AuthenticationResult']['IdToken'];
-    axios.defaults.headers.common['accept'] = 'application/json';
-    axios.defaults.headers.common['aud'] = CLIENT_ID;
     const instance = axios.create();
-    const response = await instance.get('/');
-    console.log(response.data);
 
+    //1- Get the list of pictures
+    const responseGet = await instance.get('/', {
+        baseURL: API_URL,
+        headers: apiHeaders,
+    });
+    if (responseGet.status != 200) {
+        console.log(responseGet);
+        throw new Error("Error while getting the list of unicorn pics");
+    }
+    //2- Upload a unicorn pic
+    const responsePrepPost = await instance.put('/preparepost', {user: username}, {
+        baseURL: API_URL,
+        headers: apiHeaders,
+    });
+    if ((responsePrepPost.status != 200) || (!responsePrepPost.data['url'])) {
+        console.log(responsePrepPost);
+        throw new Error("Error while getting the URL to post pic");
+    }
+    const postPicUrl = responsePrepPost.data['url'];
+    const picObject = await s3.getObject({
+        Bucket: PICTURE_BUCKET,
+        Key: PICTURE_KEY
+    }).promise();
+    const responsePost = await instance.put(postPicUrl, picObject.Body, {
+        headers: {
+            'Content-Type': 'image/png' ,
+            'x-amz-meta-userid': responsePrepPost.data['userid'],
+            'x-amz-meta-postid': responsePrepPost.data['postid'],
+            'x-amz-meta-createdat': responsePrepPost.data['createdat'],
+            'x-amz-meta-ownername': responsePrepPost.data['ownername'],
+        },
+    });
+    if (responsePost.status != 200) {
+        console.log(responsePost);
+        throw new Error("Error while posting pic");
+    }
+    console.log("Pic uploaded");
     return {
         "statusCode": 200,
     }
--- source/5-unicornPics/backend/lib/loadtesting/loadtesting.ts
+++ source/5-unicornPics/backend/lib/loadtesting/loadtesting.ts
@@ -3,6 +3,8 @@ import * as lambda from '@aws-cdk/aws-lambda-nodejs';
 import * as iam from '@aws-cdk/aws-iam';
 import { Auth } from '../common/auth';
 import { PostsService } from '../postsService/posts-service';
+import * as assets from '@aws-cdk/aws-s3-assets';
+import * as path from 'path';
 
 interface LoadtestingProps {
   userAuth: Auth
@@ -13,20 +15,31 @@ export class Loadtesting extends cdk.Construct {
   constructor(scope: cdk.Construct, id: string, props: LoadtestingProps) {
     super(scope, id);
 
+    const unicornPic = new assets.Asset(this, 'UnicornPic', {
+      path: path.resolve(__dirname, 'unicorn.png'),
+    });
+
     const triggerLoadTestRole = new iam.Role(this, 'triggerLoadTestRole', {
       assumedBy: new iam.ServicePrincipal('lambda.amazonaws.com'),
       roleName: "TriggerLoadTestRole"
     });
     triggerLoadTestRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("service-role/AWSLambdaBasicExecutionRole"));
     triggerLoadTestRole.addManagedPolicy(iam.ManagedPolicy.fromAwsManagedPolicyName("AmazonCognitoPowerUser"));
+    triggerLoadTestRole.addToPolicy(new iam.PolicyStatement({
+      actions: ["s3:GetObject","s3:GetObjectVersion"],
+      resources: [unicornPic.bucket.arnForObjects(unicornPic.s3ObjectKey)],
+    }));
 
     const triggerLoadTest = new lambda.NodejsFunction(this, 'triggerLoadTest', {
       environment: {
         USER_POOL_ID: props.userAuth.userPool.userPoolId,
         CLIENT_ID: props.userAuth.userPoolClient.userPoolClientId,
-        API_URL: props.postService.postsApi.url
+        API_URL: props.postService.postsApi.url,
+        PICTURE_BUCKET: unicornPic.s3BucketName,
+        PICTURE_KEY: unicornPic.s3ObjectKey,
       },
-      role: triggerLoadTestRole
+      role: triggerLoadTestRole,
+      timeout: cdk.Duration.minutes(5),
     });
   }
 }
